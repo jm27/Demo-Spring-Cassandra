e5cf0d2f9dc070404be3c58455dc33183d9b5366
jm27 <jme2791@gmail.com>
2021-08-25 20:12:14 -0700

    Spring cloud, discovery and gateway

---
+++ gateway-demo/src/main/java/com/example/gatewaydemo/gdemo/GatewayDemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.gatewaydemo.gdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayDemoApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route( p -> p
+						.path("/items")
+						.uri("http://host.docker.internal:51491/items"))
+						.build();
+	}
+
+}
---
+++ gateway-demo/src/test/java/com/example/gatewaydemo/gdemo/GatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaydemo.gdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
